1293991437efdfd8038ceabea3158293ce4e20e9
Jerry_Wu <409187100@qq.com>
2025-09-25 17:04:29 +0800

    refactor: add server-side check for AsyncLocalStorage initialization in async-request-store

--- packages/qwik-router/src/middleware/request-handler/async-request-store.ts
+++ packages/qwik-router/src/middleware/request-handler/async-request-store.ts
@@ -1,19 +1,22 @@
+import { isServer } from 'packages/qwik/dist';
 import type { RequestEventInternal } from './request-event';
 import type { AsyncLocalStorage } from 'node:async_hooks';
 
 export type AsyncStore = AsyncLocalStorage<RequestEventInternal>;
 
-import('node:async_hooks')
-  .then((module) => {
-    const AsyncLocalStorage = module.AsyncLocalStorage;
-    asyncRequestStore = new AsyncLocalStorage<RequestEventInternal>();
-  })
-  .catch((err) => {
-    console.warn(
-      'AsyncLocalStorage not available, continuing without it. This might impact concurrent server calls.',
-      err
-    );
-  });
+if (isServer) {
+  import('node:async_hooks')
+    .then((module) => {
+      const AsyncLocalStorage = module.AsyncLocalStorage;
+      asyncRequestStore = new AsyncLocalStorage<RequestEventInternal>();
+    })
+    .catch((err) => {
+      console.warn(
+        'AsyncLocalStorage not available, continuing without it. This might impact concurrent server calls.',
+        err
+      );
+    });
+}
 
 // Qwik Core will also be using the async store if this is present
 export let asyncRequestStore: AsyncStore | undefined = undefined;
